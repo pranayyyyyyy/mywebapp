6045d1921c4cb0e7279edc9fb3610f2e9da660de
Pranay Abhang <abhangpranay4014@gmail.com>
2025-05-15 11:49:29 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Azure Web App</title>
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Welcome to Azure Web App!</h1>
+        <p>This PHP app is running on Azure.</p>
+        <p>Today's date is: <?php echo date("Y-m-d"); ?></p>
+    </div>
+</body>
+</html>
